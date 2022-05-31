52f81dbc7478da2b1da9d4bbfa2816e6ce06e842
Israel Palma <israelpalma@MacBook-Pro-de-Israel.local>
2022-05-31 12:50:36 -0400

    error nullPointerException

--- src/com/generation/f20220531/Main.java
+++ src/com/generation/f20220531/Main.java
@@ -1,57 +1,54 @@
 package com.generation.f20220531;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner sc =new  Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		// instancia de cliente
 		Cliente cliente = new Cliente();
 		cliente.setId(1234);
 		cliente.setNombre("Donato");
 		cliente.setRut("123456789-0");
-		
-		//excepciones
+
+		// excepciones
 		Cliente clienteDatosDinamicos = new Cliente();
-		
+
 		System.out.println("Ingrese el id de Cliente");
-		//Integer id = sc.nextInt();
+		///Integer id2 = sc.nextInt();
 		String idString = sc.nextLine();
-		
+
 		try {
-			Integer id = Integer.parseInt(idString);
-			clienteDatosDinamicos.setId(id);
+			Integer id = null;//Integer.parseInt(idString);
+			clienteDatosDinamicos.setId(id+1);
+			sc.nextLine();
+
+			System.out.println("Ingrese el nombre de Cliente");
+			String nombre = sc.nextLine();
+			clienteDatosDinamicos.setNombre(nombre);
+
+			// rut
+			System.out.println("Ingrese el rut de Cliente");
+			String rut = sc.nextLine();
+			clienteDatosDinamicos.setRut(rut);
+			// correo
+			System.out.println("Ingrese el correo de Cliente");
+			String correo = sc.nextLine();
+			clienteDatosDinamicos.setCorreo(correo);
+		} catch(NullPointerException npe) {
+			System.out.println("No se puede realizar la operacion matematica, un dato es nulo");
+		} catch (InputMismatchException ime) {
+			System.out.println("Error en el ingreso de un dato");
+		} catch (NumberFormatException nfe) {
+			System.out.println("No puede ingresar un letra como identificador " + nfe);
 		} catch (Exception e) {
-			System.out.println("No puede ingresar un letra como identificador");
+			System.out.println("Ha ocurrido un error, contecte al administrador " + e);
 		}
-		
-		sc.nextLine();
-		
-		
-		
-		System.out.println("Ingrese el nombre de Cliente");
-		String nombre = sc.nextLine();
-		clienteDatosDinamicos.setNombre(nombre);
-		
-		//rut
-		System.out.println("Ingrese el rut de Cliente");
-		String rut = sc.nextLine();
-		clienteDatosDinamicos.setRut(rut);
-		//correo
-		System.out.println("Ingrese el correo de Cliente");
-		String correo = sc.nextLine();
-		clienteDatosDinamicos.setCorreo(correo);
-		
-		
-		
-		
-		
-		
-		//arreglo de clientes
-		
-		
-		
+
+		// arreglo de clientes
+
 	}
 
 }
