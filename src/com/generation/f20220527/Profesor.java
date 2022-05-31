6c092810b0d610767b0823748ffed3827e434312
Israel Palma <israelpalma@MacBook-Pro-de-Israel.local>
2022-05-31 09:59:28 -0400

    modificadores

--- src/com/generation/f20220527/Profesor.java
+++ src/com/generation/f20220527/Profesor.java
@@ -1,5 +1,17 @@
 package com.generation.f20220527;
 
 public class Profesor {
+	
+	String nombre;
+	String apellido;
+	
+	Boolean profesorJefe = false;
+	Boolean profesorAyudante = true;
+	
+	public static void impartirClases() {
+		
+		profesorJefe= true;
+	}
+	
 
 }
---
+++ src/com/generation/f20220530/Main.java
@@ -0,0 +1,45 @@
+package com.generation.f20220530;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		List<Alumno> listaAlumnos = new ArrayList<Alumno>();
+		
+		
+		// instancia de una clase
+		Alumno alumno = new Alumno(); 
+		Alumno alumno2 = new Alumno("Leonardo","Utreras",26,"G1");
+		
+		listaAlumnos.add(alumno);
+		listaAlumnos.add(alumno2);
+		
+		
+		alumno.setNombre("Diana");
+		
+		System.out.println(alumno.getNombre());//null
+		System.out.println(alumno2.getNombre());
+		
+		//setter
+		alumno.setNombre("Israel");
+		System.out.println(alumno.getNombre());
+		
+		System.out.println(alumno2.toString());
+		
+		for (int i = 0; i < listaAlumnos.size(); i++) {
+			System.out.println(listaAlumnos.get(i).getNombre());
+			System.out.println(listaAlumnos.get(i).getApellido());
+			System.out.println(listaAlumnos.get(i).toString());
+		}
+		
+		for (Alumno objetoAlumno : listaAlumnos) {
+			/*System.out.println(objetoAlumno.getNombre());
+			System.out.println(objetoAlumno.getCurso());*/
+			System.out.println(objetoAlumno.toString());
+		}
+
+	}
+
+}
